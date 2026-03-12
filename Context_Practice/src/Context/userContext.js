79151658075dc3fa5dccad1b3d4f2d4c450fb0ca
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-12 23:41:21 +0600

    I am practicing the concept of Context okay

---
+++ Context_Practice/src/Context/userContext.js
@@ -0,0 +1,5 @@
+import { useContext } from "react";
+
+let UserContext = useContext();
+
+export default UserContext;
---
+++ Context_Practice/src/Context/userContextProvider.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import UserContext from "./userContext";
+
+const userContextProvider = ({ children }) => {
+  const [user, setUser] = useState("");
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
