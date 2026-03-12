a255f1a911731296f89de3b5bea6c241198637f2
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-12 23:43:24 +0600

    I have added component counter.jsx

--- Context_Practice/src/App.jsx
+++ Context_Practice/src/App.jsx
@@ -1,12 +1,9 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  
-  return (
-    <div>
+  return <div>
+    
+  </div>;
+};
 
-    </div>
-  )
-}
-
-export default App
+export default App;
