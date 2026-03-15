a42aa49c86027fa1d299a51934042bf8633c479d
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-16 00:03:42 +0600

    I have created 2 component name Card and Cart okay

--- Recover/src/App.jsx
+++ Recover/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Cart from "./Components/Cart"
+import Card from "./Components/Card"
 
 const App = () => {
   return (
     <div>
-      
+      <Card/>
     </div>
   )
 }
